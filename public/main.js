82b2fe03a854b72061da818a79baa9163bc3d14c
zerop <zerop.development@gmail.com>
2022-08-05 02:56:07 +0300

    end

--- public/main.js
+++ public/main.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('xwz');
+/**      signature*/
+/**      implement */
+function readArguments(args) {
+    const result = [];
+    for (let i = 2; i < process.argv.length; i++) {
+        result.push(process.argv[i]);
+    }
+    return result;
+}
+module.exports = readArguments;
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
-console.log('xwz')
+
+
+
+/**      signature*/
+function readArguments (args: Array<string>): Array<string>
+/**      signature*/
+
+
+
+/**      implement */
+function readArguments (args: Array<string>): Array<string> {
+
+    const result: Array<string> = []
+
+    for (let i = 2; i < process.argv.length; i++) {
+        result.push(process.argv[i])
+    }
+
+    return result
+}
+
+module.exports = readArguments
+
